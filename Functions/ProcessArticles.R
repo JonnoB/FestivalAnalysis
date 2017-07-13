85785255ec13fe4ea90cc7cd8f17fe94a8f16d5f
Jonathan Bourne <cheg_a_leg@hotmail.com>
2017-07-13 23:07:20 +0100

    Process article
    Made a function that processes all the html into finalised data sets for analysis.
    Also perform a PCA on the sentiment vectors

---
+++ Functions/ProcessArticles.R
@@ -0,0 +1,96 @@
+ProcessArticles<- function(FestivalName){
+  #This function loads and processes the corpus from nexislexis before outputting the analysed results
+  
+  
+  festlower <- tolower(FestivalName)
+  festspaceless <- gsub(" ", "",festlower)
+  
+  #Loads all files with the festival name into a list
+  corpus<- list.files(pattern = FestivalName) %>% map(~LexisNexisSource(.x, encoding = "UTF-8") %>%
+                                                        Corpus(., readerControl = list(language = NA))
+  ) %>%
+    #Collapses list of corpora into a single corpus
+    do.call(function(...) c(..., recursive = FALSE), .)
+  
+  
+  Papernames<- sapply(1:length(corpus),function(n) meta(corpus[[n]])$origin)
+  
+  Papernames <- Papernames %>% tolower %>%
+    # gsub("\\(london\\)|\\(england\\)|\\(united kingdom\\)", "",.) %>%
+    gsub("sunday", "", .) %>% 
+    gsub(".*sun.*", "sun", .) %>%
+    gsub(".*express.*", "express", .) %>%
+    gsub(".*independent.*", "independent", .) %>%
+    gsub(".*telegraph.*", "telegraph", .) %>%
+    gsub(".*mirror.*", "mirror", .) %>%  
+    gsub(".*times.*", "times", .) %>%
+    gsub(".*mail.*", "mail", .) %>%
+    gsub(".*observer.*", "guardian", .) %>%
+    gsub(".*guardian.*", "guardian", .)
+  str_trim
+  Papernames %>% unique
+  
+  for (i in 1:length(corpus)) {
+    meta(corpus[[i]])$origin <- Papernames[i]
+  }
+  
+  corpus <- corpus %>%
+    tm_map(., content_transformer(tolower)) %>%
+    tm_map( PatternReplace, "[[:punct:]]", "") %>%
+    tm_map( PatternReplace, "festival", " festival ") %>%   
+    tm_map( PatternReplace, "carnival", " carnival ") %>%   
+    tm_map( PatternReplace, festlower, festspaceless) %>%
+    tm_map( removeWords, stopwords("english")) %>%
+    tm_map(stripWhitespace)  
+  
+  
+  
+  #keep only articles that mention the carnival more than once, as others are probably just mentioning it in passing
+  dtm <- DocumentTermMatrix(corpus)
+  keep <- dtm %>% #[, grepl("carnival$", dtm$dimnames$Terms)] %>% 
+    tidy 
+  
+  Docnames <- data_frame(document=(unique(keep$document)))
+  
+  keep<-  keep %>% filter(grepl(paste0(festspaceless,"$"), term)) %>% left_join(Docnames,., by = "document") %>%
+    mutate(keep = count>1)  %>%
+    arrange(match(document,Docnames$document))
+  
+  keep<-  keep %>% filter(grepl(paste0(festspaceless,"$"), term))%>%
+    mutate(keep = count>1)
+  
+  
+  #after this sentiment analysis can be done
+  corpus3 <- corpus 
+  
+  #words cleaned and with the paper name
+  CleanWords <-corpus3 %>% DocumentTermMatrix %>% tidy %>%
+    filter(document %in% keep$document) %>%
+    left_join(., tidy(corpus3) %>% 
+                select(id, origin), by= c("document" = "id")) %>%
+    rename(Paper = origin)
+  
+  #sentiment attached to the words
+  SentiWords <- CleanWords %>%
+    right_join(get_sentiments("nrc"), by=c("term"="word")) %>%
+    filter(complete.cases(.)) 
+  
+  #sentmiment by paper
+  SentimentbyPaper<- SentiWords%>%
+    group_by(Paper, sentiment) %>%
+    summarise(count = sum(count, na.rm = T)) %>%
+    spread(key = Paper, value = count)
+  SentimentbyPaper[is.na(SentimentbyPaper)] <-0
+  
+  
+  #Carnival Sentiment
+  FestivalSentiment<- SentiWords%>%
+    group_by(sentiment) %>%
+    summarise(count = sum(count, na.rm = T)) %>%
+    mutate(Festival = FestivalName)
+  
+  out <- list(SentimentbyPaper, FestivalSentiment) %>% 
+    setNames(c("SentimentbyPaper", "FestivalSentiment"))
+  
+  
+}
